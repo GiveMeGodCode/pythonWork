44659b3c0a81e5ffc88a5be2b96102ccb74d8c88
giveMeGodCode <lee8307@gmail.com>
2021-11-02 15:08:02 +0900

    실습소스

---
+++ rpa/2_desktop/6_image_recognition.py
@@ -0,0 +1,24 @@
+from PIL.ImageOps import grayscale
+import pyautogui
+# file_menu = pyautogui.locateOnScreen("file_menu.png")
+# print(file_menu)
+# pyautogui.click(file_menu)
+
+
+# alarm_icon = pyautogui.locateOnScreen("alarm.png")
+# pyautogui.moveTo(alarm_icon)
+
+
+# for i in pyautogui.locateAllOnScreen("check_box.png"):
+#     print(i)
+#     pyautogui.click(i)
+
+
+# check_box = pyautogui.locateOnScreen("alarm.png")
+# pyautogui.click(check_box)
+
+
+# GrayScale.
+
+check_box = pyautogui.locateOnScreen("alarm.png", grayscale=true)
+pyautogui.click(check_box)
