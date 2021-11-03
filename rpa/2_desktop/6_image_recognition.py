e4d2e9c8be1a049fad8e313d460e0066d3939538
giveMeGodCode <lee8307@gmail.com>
2021-11-03 09:14:20 +0900

    추가파일

--- rpa/2_desktop/6_image_recognition.py
+++ rpa/2_desktop/6_image_recognition.py
@@ -1,3 +1,5 @@
+import sys
+import time
 from PIL.ImageOps import grayscale
 import pyautogui
 # file_menu = pyautogui.locateOnScreen("file_menu.png")
@@ -19,7 +21,35 @@ import pyautogui
 
 
 # GrayScale.
+# check_box = pyautogui.locateOnScreen("alarm.png", grayscale=True, region)
+# check_box = pyautogui.locateOnScreen(
+#     "trash_icon.png", region=(1488, 623, 1881-1488, 137))
+# pyautogui.moveTo(check_box)
 
-check_box = pyautogui.locateOnScreen("alarm.png", grayscale=True)
-pyautogui.click(check_box)
-pyautogui.click(check_box)
+
+# run_btn = pyautogui.locateOnScreen("run_btn.png", confidence=0.7)
+# pyautogui.moveTo(run_btn)
+
+
+# 바로 안보일때
+file_menu_notepad = pyautogui.locateOnScreen("file_menu_notepad.png")
+# if file_menu_notepad:
+#     pyautogui.click(file_menu_notepad)
+# else:
+#     print("발견실패")
+
+# while file_menu_notepad is None:
+#     file_menu_notepad = pyautogui.locateOnScreen("file_menu_notepad.png")
+#     print("발견실패")
+
+
+timeout = 10
+start = time.time()
+# file_menu_notepad = pyautogui.locateOnScreen("file_menu_notepad.png")
+file_menu_notepad = None
+while file_menu_notepad is None:
+    file_menu_notepad = pyautogui.locateOnScreen("file_menu_notepad.png")
+    end = time.time()  # 종료시간
+    if end-start > timeout:
+        print("시간초과")
+        sys.exit()
